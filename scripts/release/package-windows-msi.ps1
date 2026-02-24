7685d7521621a86334d47bed75deafdfd83a64ea
Cédric Michaux <cedric@fullfrontend.be>
2026-02-24 20:09:12 +0100

    ci(release): fix wix ice80 by marking 64-bit components

--- scripts/release/package-windows-msi.ps1
+++ scripts/release/package-windows-msi.ps1
@@ -12,6 +12,10 @@ $msiVersion = "$($nums[0]).$($nums[1]).$($nums[2])"
 
 $archRaw = ($env:RUNNER_ARCH ?? 'X64').ToLowerInvariant()
 $arch = if ($archRaw -eq 'x64') { 'x64' } elseif ($archRaw -eq 'arm64') { 'arm64' } else { $archRaw }
+$is64Bit = @('x64', 'arm64') -contains $arch
+$wixPlatform = if ($is64Bit) { $arch } else { 'x86' }
+$programFilesFolder = if ($is64Bit) { 'ProgramFiles64Folder' } else { 'ProgramFilesFolder' }
+$componentWin64 = if ($is64Bit) { 'yes' } else { 'no' }
 
 $outDir = "release-assets"
 $wxsPath = Join-Path $outDir "retaia-agent.wxs"
@@ -31,24 +35,24 @@ New-Item -ItemType Directory -Path $outDir -Force | Out-Null
     Version="$msiVersion"
     Manufacturer="Retaia"
     UpgradeCode="8B4AEE21-A9C4-45E8-B2A2-86A1272AE947">
-    <Package InstallerVersion="500" Compressed="yes" InstallScope="perMachine" />
+    <Package InstallerVersion="500" Compressed="yes" InstallScope="perMachine" Platform="$wixPlatform" />
     <MajorUpgrade DowngradeErrorMessage="A newer version of Retaia Agent is already installed." />
     <MediaTemplate />
 
     <Directory Id="TARGETDIR" Name="SourceDir">
-      <Directory Id="ProgramFiles64Folder">
+      <Directory Id="$programFilesFolder">
         <Directory Id="INSTALLFOLDER" Name="RetaiaAgent" />
       </Directory>
     </Directory>
 
     <DirectoryRef Id="INSTALLFOLDER">
-      <Component Id="cmpAgentCtl" Guid="13D43666-D83A-4067-BB20-B2A638D8D0E6">
+      <Component Id="cmpAgentCtl" Guid="13D43666-D83A-4067-BB20-B2A638D8D0E6" Win64="$componentWin64">
         <File Id="filAgentCtl" Source="$binDir\\agentctl.exe" KeyPath="yes" />
       </Component>
-      <Component Id="cmpAgentRuntime" Guid="B15FA8E3-8607-4A71-8F8B-8125D784F315">
+      <Component Id="cmpAgentRuntime" Guid="B15FA8E3-8607-4A71-8F8B-8125D784F315" Win64="$componentWin64">
         <File Id="filAgentRuntime" Source="$binDir\\agent-runtime.exe" KeyPath="yes" />
       </Component>
-      <Component Id="cmpDesktopShell" Guid="D6CB1AB2-5039-4A0E-9355-D61545507183">
+      <Component Id="cmpDesktopShell" Guid="D6CB1AB2-5039-4A0E-9355-D61545507183" Win64="$componentWin64">
         <File Id="filDesktopShell" Source="$binDir\\agent-desktop-shell.exe" KeyPath="yes" />
       </Component>
     </DirectoryRef>
